9566595d046a8d2a9133dc001cf5b8fb31f2951a
paulagardner <paula.dorothyg@gmail.com>
2021-09-28 12:08:14 -0700

    committing original working version

--- harpak_przeworski.py
+++ harpak_przeworski.py
@@ -117,22 +117,6 @@ def write_treefile(pop: fwdpy11.DiploidPopulation, args: argparse.Namespace):
     ts.dump(args.treefile)
 
 
-ts = fwdpy11.tskit_tools.load(
-    sys.argv[1]
-)  # calling sys.argv[1] is because treefile is our 'first' command line argument as defined in make_parser, right?
-
-ind_md = ts.decode_individual_metadata()
-
-fitness = np.zeros(len(ind_md))
-phenotype = np.zeros(len(ind_md))
-
-for i, md in enumerate(ind_md):
-    fitness[i] = md.w
-    phenotype[i] = md.g + md.e
-
-print(f"Mean fitness = {fitness.mean()}.  Mean phenotype = {phenotype.mean()}")
-
-
 if __name__ == "__main__":
     # build our parser
     parser = make_parser()
