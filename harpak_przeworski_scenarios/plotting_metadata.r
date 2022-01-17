a4bab21c73268f533674ac86217f36e2f0925ee0
paulagardner <paula.dorothyg@gmail.com>
2022-01-17 10:54:27 -0800

    sample files and scripts for simulating harpak+przeworski w/o ancestry info

--- harpak_przeworski_scenarios/plotting_metadata.r
+++ harpak_przeworski_scenarios/plotting_metadata.r
@@ -2,28 +2,21 @@
 # directory if using the r console on vscode.
 
 
+# setwd("/home/paula/fwdpy11/polygenic_effect_sims/harpak_przeworski_scenarios")#of course, you don't want to have to change this for every machine you're running on.
+
+# setting working directory to be whatever directory the file is in:
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # https://stackoverflow.com/questions/3452086/getting-path-of-an-r-script/35842176#35842176
 
 
-
-#setwd("/home/paula/fwdpy11/polygenic_effect_sims/harpak_przeworski_scenarios")#of course, you don't want to have to change this for every machine you're running on. 
-
-#setting working directory to be whatever directory the file is in: 
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #https://stackoverflow.com/questions/3452086/getting-path-of-an-r-script/35842176#35842176
+metadata <- read.table("harpak_przeworski_case3.txt", header = TRUE, sep = "\t")
 
 
-metadata <- read.table("harpak_przeworski.txt", header = TRUE, sep = "\t")
-# tried fill = TRUE here, and while it
-# does remove the scan error, it doesn't change that
-# metadata, when called, shows as 'not found'
-
-metadata
- 
-#plot(metadata$ind_genetic_value)
 library("ggplot2")
-ggplot(metadata, aes(x=ind_genetic_value, group = count)) + geom_density()
-ggplot(metadata, aes(x=ind_fitness, group = count)) + geom_density()
-ggplot(metadata, aes(x=ind_environmental_value, group = count)) + geom_density()
-ggplot(metadata, aes(x=ind_genetic_value, y=ind_fitness)) + geom_point()
-ggplot(metadata, aes(x=ind_phenotype, y=ind_fitness, group = count)) + geom_density()
-       
-       
+ggplot(metadata, aes(x = (ind_genetic_value + ind_environmental_value), y = ind_fitness, color = count)) +
+    geom_point()
+
+ggplot(metadata, aes(x = ind_environmental_value, group = count, color = count)) +
+    geom_density()
+
+ggplot(metadata, aes(x = ind_genetic_value, group = count, color = count)) +
+    geom_density()
